17bc000c5f3d17f28b9467e04eed6233fb4abde6
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-01 14:45:45 +0900

    Update script.js
    js code being added

---
+++ script.js
@@ -0,0 +1,9 @@
+function nameAndColor () {
+    let username = prompt("What is your name? ");
+    let usercolor = prompt("What is your favorite color? ");
+    document.write ("Hello");
+    document.write (username);
+    document.write ("! Since your favorite color is ");
+    document.write (usercolor);
+    document.write (", you have won a " + usercolor + "phonecase!"); 
+}
